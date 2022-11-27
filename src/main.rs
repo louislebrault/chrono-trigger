a4bee22ddcd4d6aec8f53fe75a338ed28eb09eb5
Louis Lebrault <louis.lebrault@gmail.com>
2022-11-27 02:48:51 +0100

    channel and status

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,59 @@ use std::io;
 use std::thread;
 use std::time;
 use std::io::Write;
+use std::sync::mpsc;
+use std::sync::mpsc::Sender;
+
+const ONE_MINUTE_MS : u64 = 60 * 1000;
+const TIMER : i32 = 25;
+
+enum Status {
+    Timer,
+    Pause
+}
+
+// https://doc.rust-lang.org/rust-by-example/std_misc/channels.html
 
 fn main() {
-    const ONE_MINUTE_MS : u64 = 60 * 1000;
-    const TIMER : i32 = 25;
+    let mut status = Status::Timer;
+    let (tx, rx) = mpsc::channel::<i32>();
 
-    listen_input();
+    listen_input(tx);
+    let timer_thread = start_timer();
 
+    // memory leak, from that loop i guess
+    loop {
+        match rx.recv() {
+            Ok(_) => {
+                match status {
+                    Status::Timer => {
+                        print!("\rTimer stopped, starting pause");
+                        io::stdout().flush().expect("flush failed");
+                        status = Status::Pause;
+                    },
+                    Status::Pause => {
+                        print!("\rPause stopped, starting timer");
+                        io::stdout().flush().expect("flush failed");
+                        status = Status::Timer;
+                    }
+                }
+            }
+            Err(_) => {
+                println!("Terminating.");
+            }
+        }
+    }
+}
+
+fn start_timer() -> thread::JoinHandle<()> {
+  let thread = thread::spawn(move || {
     for minutes_left in 0 .. TIMER {
         update_time(TIMER - minutes_left);
         sleep(ONE_MINUTE_MS);
     }
+  });
+
+  return thread
 }
 
 fn sleep(ms: u64) {
@@ -27,16 +69,12 @@ fn update_time(time_left: i32) {
     io::stdout().flush().expect("flush failed");
 }
 
-fn listen_input() {
-  thread::spawn(move || {
+fn listen_input(tx: Sender<i32>) {
+  thread::spawn(move || loop {
       let mut buffer = String::new();
       let stdin = io::stdin();
       stdin.read_line(&mut buffer);
-      print!("\rTimer stopped, starting pause");
-      io::stdout().flush().expect("flush failed");
-      // stop timer thread (for loop should be in a thread that we pass to this function as
-      // parameter)
-      // start pause loop thread
+      tx.send(1);
       Ok::<(), ()>(());
   });
 }
